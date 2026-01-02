aded7f950a10f2740148c6fb106f87509f9ba778
valery <vagechirkov@gmail.com>
2026-01-02 16:36:21 +0100

    update initial exploration of the parameters

--- ea_gpu_driver_scioi.sh
+++ ea_gpu_driver_scioi.sh
@@ -5,7 +5,7 @@ target_speed_array=(0.05 0.1 0.3 0.5)
 
 for dim in "${dims_array[@]}"; do
     for t_speed in "${target_speed_array[@]}"; do
-        sbatch ea_cpu_hpc_itb.sh "$t_speed" "$dim"
+        sbatch ea_gpu_hpc_scioi.sh "$t_speed" "$dim"
         sleep 1
     done
 done
