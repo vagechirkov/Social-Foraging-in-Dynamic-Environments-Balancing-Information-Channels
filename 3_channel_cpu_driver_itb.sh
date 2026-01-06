599802228bd081f880049f14e3d1e3b42f0057b5
valery <vagechirkov@gmail.com>
2026-01-06 14:06:23 +0100

    3 channel exploration on cluster code

---
+++ 3_channel_cpu_driver_itb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+target_speed_array=(0.1 0.3 0.5)
+gamma_bel_array=(1.0 2.0 5.0)
+beta_bel_array=(1.0 0.1)
+
+for beta_bel in "${beta_bel_array[@]}"; do
+    for gamma_bel in "${gamma_bel_array[@]}"; do
+        for t_speed in "${target_speed_array[@]}"; do
+            sbatch ea_cpu_hpc_itb.sh "$t_speed" "$gamma_bel" "$beta_bel"
+        done
+    done
+done
---
+++ 3_channel_cpu_hpc_itb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=3-channel-exploration            # Job name
+#SBATCH --output=job_%j.log                         # Output log file (%j will be replaced with job ID)
+#SBATCH --error=job_%j.log                          # Error log file
+#SBATCH --partition=long                            # Partition to submit to
+#SBATCH --nodes=1                                   # Ensure it runs on one node
+#SBATCH --ntasks=1                                  # Run a single task
+#SBATCH --cpus-per-task=64                          # Request 64 CPUs
+#SBATCH --mem=128G                                   # Memory allocation
+
+
+source .venv/bin/activate
+
+# add current directory to python path
+export PYTHONPATH=$PYTHONPATH:.
+
+t_speed=$1
+gamma_bel=$2
+beta_bel=$3
+
+uv run abm/3_channel_abm_exploration.py \
+    --n_agents 10 \
+    --max_steps 1000 \
+    --replicates 100 \
+    --run_name "01_2026" \
+    --target_speed "$t_speed" \
+    --social_trans_scale "$gamma_bel" \
+    --dist_noise_scale_soc "$beta_bel"
