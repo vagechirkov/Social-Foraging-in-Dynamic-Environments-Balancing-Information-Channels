692e68b54621655c8e0233d4cab8d3b7b3779396
valery <vagechirkov@gmail.com>
2026-01-09 12:01:40 +0100

    explore effects of target persistence and belief cost

--- 3_channel_cpu_driver_itb.sh
+++ 3_channel_cpu_driver_itb.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-belief_selectivity_array=(3.0)
-gamma_belief_array=(0.1 1 10)
-dim_array=(5 15)  # 10 15
+belief_selectivity_array=(1.0 3.0 6.0)
+gamma_belief_array=(0.01 0.1)  #  1 10
+dim_array=(10)  # 5  10 15
 n_agent_array=(10)  # 20 30
-target_speed_array=(0.1 0.3 0.5)
+target_speed_array=(0.1)  #  0.3 0.5
 cost_priv=(0.9)  # 0.5 0.1 0.02
-cost_belief=(0.1)  # 0.5 0.1 0.02
-base_noise_array=(1 5)
-dist_noise_scale_priv_array=(2 4 6)
-target_persistence_array=(5 10 20 40)
+cost_belief=(0.005 0.01 0.05)  # 0.5 0.1 0.02
+base_noise_array=(1)
+dist_noise_scale_priv_array=(4 6)  # 2 4 6
+target_persistence_array=(1 5)
 
 for target_persistence in "${target_persistence_array[@]}"; do
     for dist_noise_scale_priv in "${dist_noise_scale_priv_array[@]}"; do
