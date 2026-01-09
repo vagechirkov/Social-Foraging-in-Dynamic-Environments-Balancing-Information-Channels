db93bfafe211987e0011f20fab00c8d4b169dce4
valery <vagechirkov@gmail.com>
2026-01-09 14:22:48 +0100

    explore effects of target persistence and belief selectivity

--- 3_channel_cpu_driver_itb.sh
+++ 3_channel_cpu_driver_itb.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-belief_selectivity_array=(1.0 3.0 6.0)
-gamma_belief_array=(0.01 0.1)  #  1 10
+belief_selectivity_array=(6.0 9.0 12.0)
+gamma_belief_array=(0.01)  # 0.1 1 10
 dim_array=(10)  # 5  10 15
 n_agent_array=(10)  # 20 30
-target_speed_array=(0.1)  #  0.3 0.5
+target_speed_array=(0.05)  #  0.3 0.5
 cost_priv=(0.9)  # 0.5 0.1 0.02
-cost_belief=(0.005 0.01 0.05)  # 0.5 0.1 0.02
+cost_belief=(0.005)  # 0.01 0.05 0.5 0.1 0.02
 base_noise_array=(1)
 dist_noise_scale_priv_array=(4 6)  # 2 4 6
 target_persistence_array=(1 5)
+process_noise_scale_array=(0.05 0.1 0.01 0.2)
 
-for target_persistence in "${target_persistence_array[@]}"; do
-    for dist_noise_scale_priv in "${dist_noise_scale_priv_array[@]}"; do
-        for base_noise in "${base_noise_array[@]}"; do
-            for belief_selectivity in "${belief_selectivity_array[@]}"; do
-                for gamma_belief in "${gamma_belief_array[@]}"; do
-                    for n_agents in "${n_agent_array[@]}"; do
-                        for dim in "${dim_array[@]}"; do
-                            for t_speed in "${target_speed_array[@]}"; do
-                                for c_priv in "${cost_priv[@]}"; do
-                                    for c_bel in "${cost_belief[@]}"; do
-                                        sbatch 3_channel_cpu_hpc_itb.sh "$t_speed" "$c_priv" "$c_bel" "$dim" "$n_agents" "$gamma_belief" "$belief_selectivity" "$base_noise" "$dist_noise_scale_priv" "$target_persistence"
+for process_noise_scale  in "${process_noise_scale_array[@]}"; do
+    for target_persistence in "${target_persistence_array[@]}"; do
+        for dist_noise_scale_priv in "${dist_noise_scale_priv_array[@]}"; do
+            for base_noise in "${base_noise_array[@]}"; do
+                for belief_selectivity in "${belief_selectivity_array[@]}"; do
+                    for gamma_belief in "${gamma_belief_array[@]}"; do
+                        for n_agents in "${n_agent_array[@]}"; do
+                            for dim in "${dim_array[@]}"; do
+                                for t_speed in "${target_speed_array[@]}"; do
+                                    for c_priv in "${cost_priv[@]}"; do
+                                        for c_bel in "${cost_belief[@]}"; do
+                                            sbatch 3_channel_cpu_hpc_itb.sh "$t_speed" "$c_priv" "$c_bel" "$dim" "$n_agents" "$gamma_belief" "$belief_selectivity" "$base_noise" "$dist_noise_scale_priv" "$target_persistence" "$process_noise_scale"
+                                        done
                                     done
                                 done
                             done
--- 3_channel_cpu_hpc_itb.sh
+++ 3_channel_cpu_hpc_itb.sh
@@ -25,6 +25,7 @@ belief_selectivity_array=$7
 base_noise=$8
 dist_noise_scale_priv=$9
 target_persistence=${10}
+process_noise_scale=${11}
 
 python abm/3_channels_abm_exploration.py \
     --m n_agents="$n_agents" \
@@ -38,7 +39,7 @@ python abm/3_channels_abm_exploration.py \
     y_dim="$dim" \
     social_trans_scale="$gamma_belief" \
     belief_selectivity_threshold="$belief_selectivity_array" \
-    process_noise_scale=0.05 \
+    process_noise_scale=process_noise_scale \
     base_noise="$base_noise" \
     dist_noise_scale_priv="$dist_noise_scale_priv" \
     target_persistence="$target_persistence"
